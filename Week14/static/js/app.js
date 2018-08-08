3d905ef5ed3de87e11407aaf1391d9dad77a6b5b
Kash <ksantos79@gmail.com>
2018-08-08 13:10:27 -0700

    Week 14 completed HW (basic)

--- Week14/static/js/app.js
+++ Week14/static/js/app.js
@@ -7,35 +7,61 @@ var newDateClick = d3.select("#filter-btn");
 
 // Complete the click handler for the form
 newDateChange.on("change", function() {
+  console.log("Event: change")
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
   // Select the input element and get the raw HTML node
   let inputElement = d3.select("#datetime");
+  console.log("inputElement: "+inputElement)
   // Get the value property of the input element
   let inputValue = inputElement.property("value");
+  console.log("inputValue: "+inputValue)
   // Use the form input to filter the data by blood type
   console.log(inputValue);
   console.log(tableData);
   let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
   console.log(filteredData);
+
+  updateTable(filteredData);
   // BONUS: Calculate summary statistics for the age field of the filtered data
 
 });
 
-newDateClick.on("submit", function() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  // Select the input element and get the raw HTML node
-  let inputElement = d3.select("#datetime");
-  // Get the value property of the input element
-  let inputValue = inputElement.property("value");
-  // Use the form input to filter the data by blood type
-  console.log(inputValue);
-  console.log(tableData);
-  let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-  console.log(filteredData);
+newDateClick.on("click", function() {
+  console.log("Event: click")
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    let inputElement = d3.select("#datetime");
+    console.log("inputElement: "+inputElement)
+    // Get the value property of the input element
+    let inputValue = inputElement.property("value");
+    console.log("inputValue: "+inputValue)
+    // Use the form input to filter the data by blood type
+    console.log(inputValue);
+    console.log(tableData);
+    let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(filteredData);
+    updateTable(filteredData);
+  
   // BONUS: Calculate summary statistics for the age field of the filtered data
 
 });
+
+let tbody = d3.select("tbody");
+
+function updateTable(finalData) {
+  tbody.html("");
+
+  finalData.forEach(rowData => {
+    let row = tbody.append("tr");
+    Object.values(rowData).forEach((val) => {
+      let cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
+}
+
+updateTable(tableData);
