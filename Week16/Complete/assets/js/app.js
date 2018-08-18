e3b78fc1cf1e20d246380bc0193561d4276a0b9c
Kash <ksantos79@gmail.com>
2018-08-18 13:06:09 -0700

    Completed HW Week16

--- Week16/Complete/assets/js/app.js
+++ Week16/Complete/assets/js/app.js
@@ -22,7 +22,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets\\data\\data.csv", function(err, csvdata) {
+d3.csv("data\\data.csv", function(err, csvdata) {
   // console.log("Import Data", err)
   //if (err) throw err;
   // console.log("CSV Data:", csvdata)
@@ -91,9 +91,39 @@ d3.csv("assets\\data\\data.csv", function(err, csvdata) {
    // Step 5: Create Circles
   // ==============================
 
-  var div = d3.select("body").append("div")	
-  .attr("class", "tooltip")				
-  .style("opacity", 0);
+  // var div = d3.select("body").append("div")	
+  // .attr("class", "tooltip")				
+  // .style("opacity", 0);
+  chartGroup.selectAll("text.stateText")
+    .data(csvdata)
+    .enter()
+    .append("text")
+    .text((d) => d.abbr)
+    .attr("class", "stateText")
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare -.35))
+    // .on("mouseover", function(d) {		
+    //   div.transition()		
+    //       // .duration(500)		
+    //       .style("opacity", .9);		
+      // div.html(`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%</strong>`)	
+      //     .style("left", (d3.event.pageX)+10 + "px")		
+      //     .style("top", (d3.event.pageY) + "px")
+      //     .style("background", "rgba(0, 0, 0, 0.8)")
+      //     .style("padding", "6px")
+      //     .style("font-size", "12px")
+      //     .style("line-height", "1")
+      //     .style("line-height", "1.5em")
+      //     .style("color", "#fff")
+      //     .style("text-align", "center")
+      //     .style("text-transform", "capitalize")
+      //     .style("border-radius", "8px")
+      //     .style('display', 'block')
+      //   })					
+      //   .on("mouseout", function(d) {		
+      //     div.transition()		
+      //     //  .duration(500)		
+      //     .style("display", "none");});	;
 
   var circlesGroup = chartGroup.selectAll("circle")
   .data(csvdata)
@@ -103,7 +133,7 @@ d3.csv("assets\\data\\data.csv", function(err, csvdata) {
   .attr("cy", d => yLinearScale(d.healthcare))
   .attr("r", 15)
   .attr("fill", "lightblue")
-  .attr("opacity", ".6")
+  .attr("opacity", ".3")
   .attr("stroke-width", "1")
   .attr("stroke", "blue") 
   .attr("alt", d => d.abbr)
@@ -112,61 +142,32 @@ d3.csv("assets\\data\\data.csv", function(err, csvdata) {
   // console.log("x = ",d.abbr, " ", xLinearScale(csvdata.poverty))
   // console.log("x = ",d.abbr, " ", yLinearScale(csvdata.healthcare))
 
-  chartGroup.selectAll("text.stateText")
-    .data(csvdata)
-    .enter()
-    .append("text")
-    .text((d) => d.abbr)
-    .attr("class", "stateText")
-    .attr("x", d => xLinearScale(d.poverty))
-    .attr("y", d => yLinearScale(d.healthcare -.35))
-    .on("mouseover", function(d) {		
-      div.transition()		
-          // .duration(500)		
-          .style("opacity", .9);		
-      div.html(`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%</strong>`)	
-          .style("left", (d3.event.pageX)+10 + "px")		
-          .style("top", (d3.event.pageY) + "px")
-          .style("background", "rgba(0, 0, 0, 0.8)")
-          .style("padding", "6px")
-          .style("font-size", "12px")
-          .style("line-height", "1")
-          .style("line-height", "1.5em")
-          .style("color", "#fff")
-          .style("text-align", "center")
-          .style("text-transform", "capitalize")
-          .style("border-radius", "8px")
-          .style('display', 'block')
-        })					
-        .on("mouseout", function(d) {		
-          div.transition()		
-          //  .duration(500)		
-          .style("display", "none");});	;
+
 
 
   // Step 6: Initialize tool tip
   // ==============================
-  // var toolTip = d3.tip()
-  //   .attr("class", "d3-tip")
-  //   .html(function (d) {
-  //     return (`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%</strong>`);
-  //   });
-  //   // console.log("Step 6")
+  var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .html(function (d) {
+      return (`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%</strong>`);
+    });
+    // console.log("Step 6")
 
-  // // Step 7: Create tooltip in the chart
-  // // ==============================
-  // chartGroup.call(toolTip);
-  // // console.log("Step 7")
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  chartGroup.call(toolTip);
+  // console.log("Step 7")
 
-  // // Step 8: Create event listeners to display and hide the tooltip
-  // // ==============================
-  // circlesGroup.on("mouseover", function(d) {
-  //     toolTip.show(d);
-  //   })
-  //   // onmouseout event
-  //   .on("mouseout", function(d) {
-  //     toolTip.hide(d);
-  //   });
+  // Step 8: Create event listeners to display and hide the tooltip
+  // ==============================
+  circlesGroup.on("mouseover", function(d) {
+      toolTip.show(d,this);
+    })
+    // onmouseout event
+    .on("mouseout", function(d) {
+      toolTip.hide(d);
+    });
 
 
 });
